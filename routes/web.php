accb3945767dba8b5d004ceb3b65e0530e2bdf8f
Diogo-Mello <diogomellodacrus1108@gmail.com>
2025-09-18 10:42:26 -0300

    Aula 18-09-2025

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/about', 'sobre');
+
+Route::get('/diogo', function() {
+    echo 'Oi Diogo, lindão :)';
+});
+
+Route::get('/filmes/{filme}', function($filme) {
+    echo 'Oi Diogo, ve esse filme aqui bb :) <br> '.$filme.' gostou?';
+    echo '<script>alert("oi bb")</script>';
+});
